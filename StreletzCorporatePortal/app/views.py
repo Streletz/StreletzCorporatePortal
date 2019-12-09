d06ca3e4760fd67a4b3f20e951f972d3fb8450b2
Streletz <streletzcoder@yandex.ru>
2019-12-09 07:02:07 +0300

    Initial commit.

---
+++ StreletzCorporatePortal/app/views.py
@@ -0,0 +1,56 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+APP_NAME='Streletz Кoрпоративный Портал'
+VERSION = '0.1'
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+            'app_name':APP_NAME,
+            'version':VERSION
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+            'app_name':APP_NAME,
+            'version':VERSION
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+            'app_name':APP_NAME,
+            'version':VERSION
+        }
+    )
+
+

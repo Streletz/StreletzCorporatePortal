2406d9f603bd3a14b83896192ef1ecda247c0f97
Streletz <streletzcoder@yandex.ru>
2021-03-07 19:59:34 +0300

    0.1
     - Модели.

--- StreletzCorporatePortal/app/models.py
+++ StreletzCorporatePortal/app/models.py
@@ -3,9 +3,22 @@ Definition of models.
 """
 
 from django.db import models
+import datetime
 
-class Department(models.Model):
+class Director(models.Model):    
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
+
+class Department(models.Model):    
     name = models.CharField(max_length=255)
 
+class Employee(models.Model):   
+   name = models.CharField(max_length=255)
+   created = models.DateTimeField(default=datetime.datetime().now())
+   worksSince = models.DateTimeField()
+   dismissed = models.DateTimeField()
+   isActive = models.BooleanField(default=true)
+   department = models.ForeignKey(Department, on_delete=models.PROTECT)
 
 # Create your models here.

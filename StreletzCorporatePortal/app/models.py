56ca71dcddd9feae2e79ae5a422c2d38e5839776
Streletz <streletzcoder@yandex.ru>
2021-03-08 12:38:42 +0300

    0.1
     - Миграции;
     - Gitignore.

--- StreletzCorporatePortal/app/models.py
+++ StreletzCorporatePortal/app/models.py
@@ -4,26 +4,27 @@ Definition of models.
 
 from django.db import models
 import datetime
+class Department(models.Model):    
+    name = models.CharField(max_length=255)
+
+class Position(models.Model):    
+    name = models.CharField(max_length=255)
+
+class Employee(models.Model):   
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(default=datetime.datetime.now())
+    birthday = models.DateTimeField()
+    worksSince = models.DateTimeField()
+    dismissed = models.DateTimeField()
+    isActive = models.BooleanField(default=True)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
 
 class Director(models.Model):    
     name = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
     employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
 
-class Department(models.Model):    
-    name = models.CharField(max_length=255)
 
-class Employee(models.Model):   
-   name = models.CharField(max_length=255)
-   created = models.DateTimeField(default=datetime.datetime().now())
-   birthday = models.DateTimeField()
-   worksSince = models.DateTimeField()
-   dismissed = models.DateTimeField()
-   isActive = models.BooleanField(default=true)
-   department = models.ForeignKey(Department, on_delete=models.PROTECT)
-   position = models.ForeignKey(Position, on_delete=models.PROTECT)
-
-class Position(models.Model):    
-    name = models.CharField(max_length=255)
 
 # Create your models here.

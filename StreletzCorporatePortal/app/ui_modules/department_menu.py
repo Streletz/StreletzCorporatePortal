2c7f3ee71b3fef2eb97d7d85c2b5b61b5790a8b1
Streletz <streletzcoder@yandex.ru>
2021-04-04 14:42:06 +0300

    0.1
     - Вывод списка в меню департаментов.

--- StreletzCorporatePortal/app/ui_modules/department_menu.py
+++ StreletzCorporatePortal/app/ui_modules/department_menu.py
@@ -8,5 +8,5 @@ register = template.Library()  # тут мы присваем значение
 @register.inclusion_tag(
     '../templates/app/ui_modules/department_menu.html')
 def department_menu():
-    departments = Department.objects.all()
+    departments = Department.objects.all().order_by('name')
     return {'departments': departments}

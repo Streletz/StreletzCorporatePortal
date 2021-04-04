5a7228258ccdcca029d1e89ee2b0735747eeefa2
Streletz <streletzcoder@yandex.ru>
2021-04-04 14:38:16 +0300

    0.1
     - Заготовка меню департаментов.

--- StreletzCorporatePortal/StreletzCorporatePortal/settings.py
+++ StreletzCorporatePortal/StreletzCorporatePortal/settings.py
@@ -30,39 +30,43 @@ ALLOWED_HOSTS = []
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = ['app',
-    # Add your apps here to enable them
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',]
+                  # Add your apps here to enable them
+                  'django.contrib.admin',
+                  'django.contrib.auth',
+                  'django.contrib.contenttypes',
+                  'django.contrib.sessions',
+                  'django.contrib.messages',
+                  'django.contrib.staticfiles', ]
 
 # Middleware framework
 # https://docs.djangoproject.com/en/2.1/topics/http/middleware/
 MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',]
+              'django.contrib.sessions.middleware.SessionMiddleware',
+              'django.middleware.common.CommonMiddleware',
+              'django.middleware.csrf.CsrfViewMiddleware',
+              'django.contrib.auth.middleware.AuthenticationMiddleware',
+              'django.contrib.messages.middleware.MessageMiddleware',
+              'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 
 ROOT_URLCONF = 'StreletzCorporatePortal.urls'
 
 # Template configuration
 # https://docs.djangoproject.com/en/2.1/topics/templates/
 TEMPLATES = [{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': ['django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',],
-        },
-    },]
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': ['django.template.context_processors.debug',
+                               'django.template.context_processors.request',
+                               'django.contrib.auth.context_processors.auth',
+                               'django.contrib.messages.context_processors.messages'],
+        #                        'app.ui_modules.department_menu.department_menu', ],
+        'libraries': {
+            'department_menu': 'app.ui_modules.department_menu'
+        }
+    },
+}, ]
 
 WSGI_APPLICATION = 'StreletzCorporatePortal.wsgi.application'
 # Database
@@ -75,14 +79,14 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
- }
+    }
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [{
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+},
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
@@ -91,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [{
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },]
+    }, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
---
+++ StreletzCorporatePortal/app/ui_modules/department_menu.py
@@ -0,0 +1,12 @@
+from django import template  # нужен для регистрации шаблонного тега
+
+from app.models import Department
+
+register = template.Library()  # тут мы присваем значение
+
+
+@register.inclusion_tag(
+    '../templates/app/ui_modules/department_menu.html')
+def department_menu():
+    departments = Department.objects.all()
+    return {'departments': departments}

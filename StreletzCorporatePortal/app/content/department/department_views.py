25f60f0a514f5d218d172954a061c302b6bdd108
Streletz <streletzcoder@yandex.ru>
2021-04-18 01:06:45 +0300

    0.2.0
     - Отображение руководителя подразделения на странице подразделения;
     - Отображение сотрудников подразделения на странице подразделения.

--- StreletzCorporatePortal/app/content/department/department_views.py
+++ StreletzCorporatePortal/app/content/department/department_views.py
@@ -5,15 +5,22 @@ from app.models import Department
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
+
 
 def departmentView(request, id):
     assert isinstance(request, HttpRequest)
     department = Department.objects.get(pk=id)
+    director = None
+    if department.director_set.filter(employee__isActive=True).count() > 0:
+        director = department.director_set.filter(employee__isActive=True).first().employee
+    employees = department.employee_set.filter(isActive=True).all()
     return render(request,
-              'content/departments/view.html',
-              {
-                  'department': department,
-                  'title': department.name,
-                  'version': VERSION
-              })
+                  'content/departments/view.html',
+                  {
+                      'department': department,
+                      'title': department.name,
+                      'version': VERSION,
+                      'director': director,
+                      'employees': employees
+                  })

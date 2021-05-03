f44e21370061105f81c792e6276e1b089891a090
Streletz <streletzcoder@yandex.ru>
2021-05-03 17:12:25 +0300

    0.2.0
     - Модель для новостей.

--- StreletzCorporatePortal/app/models.py
+++ StreletzCorporatePortal/app/models.py
@@ -1,35 +1,47 @@
 """
 Definition of models.
 """
-
+from django.conf import settings
 from django.db import models
 import datetime
-class Department(models.Model):    
-    name = models.CharField(max_length=255,unique=True)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
-    #director = models.ForeignKey(Employee, on_delete=models.PROTECT)
 
-class Position(models.Model):    
-    name = models.CharField(max_length=255,unique=True)
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
 
-class Employee(models.Model):   
+
+class Employee(models.Model):
     name = models.CharField(max_length=255)
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField()
     birthday = models.DateTimeField(null=True)
-    worksSince = models.DateTimeField(default=datetime.datetime.now())
+    worksSince = models.DateTimeField()
     dismissed = models.DateTimeField(null=True)
     isActive = models.BooleanField(default=True)
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
     position = models.ForeignKey(Position, on_delete=models.PROTECT)
-    class Meta:
-        unique_together= (('name', 'birthday','department','position'),)
 
-class Director(models.Model):    
+    class Meta:
+        unique_together = (('name', 'birthday', 'department', 'position'),)
+
+
+class Director(models.Model):
     name = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
     employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
+
     class Meta:
-        unique_together= (('department','employee'),)
+        unique_together = (('department', 'employee'),)
+
+
+class Post(models.Model):
+    created = models.DateTimeField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    theme = models.CharField(max_length=255)
+    content = models.TextField(null=True)
 
 
 # Create your models here.
